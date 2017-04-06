e0b2e756f58e58fe72652d7ac600b178ca26192c
Rob Abbott <robert.abbott@habla.io>
2017-04-06 13:12:29 -0400

    Modified test to contain read and writes to table

--- src/test/dynamo_test.js
+++ src/test/dynamo_test.js
@@ -3,28 +3,30 @@ var uuid = require("uuid");
 
 AWS.config.update({
   region: "us-west-2",
-  endpoint: "http://localhost:8000"
+  endpoint: "dynamodb.us-west-2.amazonaws.com"
 });
 
 var dynamodb = new AWS.DynamoDB();
+
 var tablePrefix = 'DEV_';
 // create table
-
+/*
 var params = {
     TableName : tablePrefix + "users",
     KeySchema: [       
         { AttributeName: "partitionId", KeyType: "HASH"},  //Partition key
-        { AttributeName: "userGuid", KeyType: "RANGE" }  //Sort key
+        { AttributeName: "userId", KeyType: "RANGE" }  //Sort key
     ],
     AttributeDefinitions: [       
         { AttributeName: "partitionId", AttributeType: "N" },
-        { AttributeName: "userGuid", AttributeType: "S" }
+        { AttributeName: "userId", AttributeType: "S" }
     ],
     ProvisionedThroughput: {       
         ReadCapacityUnits: 10, 
         WriteCapacityUnits: 10
     }
 };
+
 
 dynamodb.createTable(params, function(err, data) {
     if (err) {
@@ -33,23 +35,27 @@ dynamodb.createTable(params, function(err, data) {
         console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
     }
 });
-
+*/
 
 // add document to table
-/*
-var docClient = new AWS.DynamoDB.DocumentClient()
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+
 
 var params = {
-    TableName: "users",
+    TableName: tablePrefix + "users",
     Item:{
-        "partitionId": -1,
-        "userGuid": "ea794510-cea6-4132-ae22-a7ae1d32abb1",
-        "userInfo":{
-            "emailAddress": "robert.abbott@habla.ai",
-            "displayName": "Rob Abbott"
+        partitionId: -1,
+        userId: 'ea794510-cea6-4132-ae22-a7ae1d32abb1',
+        userInfo: {
+            emailAddress: "robert.abbott@habla.ai",
+            displayName: "Rob Abbott"
         }
+
     }
 };
+
 
 console.log("Adding a new item...");
 docClient.put(params, function(err, data) {
@@ -59,5 +65,23 @@ docClient.put(params, function(err, data) {
         console.log("Added item:", JSON.stringify(data, null, 2));
     }
 });
-
 */
+
+var params = {
+    TableName: tablePrefix + "users",
+    Key:{
+        partitionId: -1,
+        userId: 'ea794510-cea6-4132-ae22-a7ae1d32abb1'
+    }
+};
+
+docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to get item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Found item:", JSON.stringify(data, null, 2));
+    }
+});
+
+
+
