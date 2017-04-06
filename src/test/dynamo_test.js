c6e61c1bc23e941c4f8d8dd2aa1e45e34a9169b1
Rob Abbott <robert.abbott@habla.io>
2017-04-06 13:35:12 -0400

    fixed syntax error in dynamo_test.js

--- src/test/dynamo_test.js
+++ src/test/dynamo_test.js
@@ -47,7 +47,7 @@ var params = {
     TableName: tablePrefix + "users",
     Item:{
         partitionId: -1,
-        userId: 'ea794510-cea6-4132-ae22-a7ae1d32abb1',
+        userId: 'ea794510-cea6-4132-ae22-a7ae1d32abb2',
         userInfo: {
             emailAddress: "robert.abbott@habla.ai",
             displayName: "Rob Abbott"
@@ -65,7 +65,7 @@ docClient.put(params, function(err, data) {
         console.log("Added item:", JSON.stringify(data, null, 2));
     }
 });
-*/
+/*
 
 var params = {
     TableName: tablePrefix + "users",
@@ -83,5 +83,5 @@ docClient.get(params, function(err, data) {
     }
 });
 
-
+*/
 
