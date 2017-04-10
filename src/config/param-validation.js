66f3b284c9a5309de71f3fdd075f8ca9ec37a46f
Anthony Daga <anthony.daga@habla.io>
2017-04-10 12:38:59 -0700

    Added validation for post apis.

--- src/config/param-validation.js
+++ src/config/param-validation.js
@@ -2,16 +2,29 @@ import Joi from 'joi';
 
 
 const validationSchemas = {
-  createUser: {
-    body: {
-      email: Joi.string().email().required()
-    }
-  },
-  registerUser: {
-    body: {
-      email: Joi.string().email().required()
-    }
-  }
+   registerUser: {
+      body: {
+         email: Joi.string().email().required()
+      }
+   },
+   createUser: {
+      body: {
+         firstName: Joi.string().min(1).required(),
+         lastName: Joi.string().min(1).required(),
+         displayName: Joi.string().min(1).required(),
+         email: Joi.string().email().required(),
+         password: Joi.string().min(1).required(),
+         country: Joi.string().min(1).required(),
+         timeZone: Joi.string().min(1).required(),
+         icon: Joi.string().base64().allow(null)
+      }
+   },
+   login: {
+      body: {
+         username: Joi.string().email().required(),
+         password: Joi.string().required()
+      }
+   }
 };
 
 export default validationSchemas;
