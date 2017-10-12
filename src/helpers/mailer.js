51a54fcd01efb981e9c438d5656e327395e7ec1a
Anthony Daga <anthony.daga@habla.io>
2017-10-12 12:44:14 -0700

    Bug fix to team room invite link.

--- src/helpers/mailer.js
+++ src/helpers/mailer.js
@@ -106,7 +106,7 @@ export function sendTeamRoomInviteToExistingUser(email, subscriberOrgName, teamN
    const html = `
       <h1><img src="https://static.wixstatic.com/media/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg/v1/fill/w_247,h_244,al_c,q_80,usm_0.66_1.00_0.01/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg" height="100" width="100" align="middle"></h1>
       <br>${byUserDisplayName} has invited you to team room "${teamRoomName}" of "${subscriberOrgName}" in Habla AI.<br>
-      <br>If reading this from a web browser, please <a href="${config.webappBaseUri}/app/acceptinvitation/?teamRoom/${webKey}">click here</a> to join them.<br>
+      <br>If reading this from a web browser, please <a href="${config.webappBaseUri}/app/acceptinvitation/teamRoom/${webKey}">click here</a> to join them.<br>
       <br>If reading this from an iOS device, please tap on the following link to join them:  hablaai://acceptinvitation/?${key}
       `;
    return sendMail({
