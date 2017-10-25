14af0ef681f6774c0395c43060589870e7a065ba
Habla-Mike <mikesomlo@gmail.com>
2017-10-25 13:14:06 -0700

    Updated links in emails since Universal Linking is now working in iOS.

--- src/helpers/mailer.js
+++ src/helpers/mailer.js
@@ -43,8 +43,7 @@ export function sendActivationLink(email, rid) {
          <img src="https://static.wixstatic.com/media/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg/v1/fill/w_247,h_244,al_c,q_80,usm_0.66_1.00_0.01/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg" height="100" width="100" align="middle" />
       </h1>
       <br>Thank you for registering for Habla AI.<br>
-      <br>If reading this from a web browser, please <a href="${config.webappBaseUri}/verifyAccount/${rid}">click here</a> to activate your account.<br>
-      <br>If reading this from an iOS device, please tap on the following link to activate your account:  hablaai://signup/${rid}
+      <br>Please <a href="${config.webappBaseUri}/verifyAccount/${rid}">click here</a> to activate your account.<br>
    `;
    return sendMail({
       from: 'habla-mailer-dev@habla.ai',
@@ -58,8 +57,7 @@ export function sendSubscriberOrgInviteToExternalUser(email, subscriberOrgName, 
    const html = `
       <h1><img src="https://static.wixstatic.com/media/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg/v1/fill/w_247,h_244,al_c,q_80,usm_0.66_1.00_0.01/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg" height="100" width="100" align="middle"></h1>
       <br>${byUserDisplayName} has invited you to "${subscriberOrgName}" in Habla AI.<br>
-      <br>If reading this from a web browser, please <a href="${config.webappBaseUri}/signup/${rid}">click here</a> to activate your account and join them.<br>
-      <br>If reading this from an iOS device, please tap on the following link to activate your account and join them:  hablaai://signup/${rid}
+      <br>Please <a href="${config.webappBaseUri}/signup/${rid}">click here</a> to activate your account and join them.<br>
    `;
    return sendMail({
       from: 'habla-mailer-dev@habla.ai',
@@ -74,8 +72,7 @@ export function sendSubscriberOrgInviteToExistingUser(email, subscriberOrgName, 
    const html = `
       <h1><img src="https://static.wixstatic.com/media/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg/v1/fill/w_247,h_244,al_c,q_80,usm_0.66_1.00_0.01/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg" height="100" width="100" align="middle"></h1>
       <br>${byUserDisplayName} has invited you to "${subscriberOrgName}" on Habla AI.<br>
-      <br>If reading this from a web browser, please <a href="${config.webappBaseUri}/app/acceptinvitation/subscriberOrg/${webKey}">click here</a> to join them.<br>
-      <br>If reading this from an iOS device, please tap on the following link to join them:  hablaai://acceptinvitation/?${key}
+      <br>Please <a href="${config.webappBaseUri}/app/acceptinvitation/subscriberOrg/${webKey}">click here</a> to join them.<br>
    `;
    return sendMail({
       from: 'habla-mailer-dev@habla.ai',
@@ -90,8 +87,7 @@ export function sendTeamInviteToExistingUser(email, subscriberOrgName, teamName,
    const html = `
       <h1><img src="https://static.wixstatic.com/media/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg/v1/fill/w_247,h_244,al_c,q_80,usm_0.66_1.00_0.01/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg" height="100" width="100" align="middle"></h1>
       <br>${byUserDisplayName} has invited you to team "${teamName}" of "${subscriberOrgName}" in Habla AI.<br>
-      <br>If reading this from a web browser, please <a href="${config.webappBaseUri}/app/acceptinvitation/team/${webKey}">click here</a> to join them.<br>
-      <br>If reading this from an iOS device, please tap on the following link to join them:  hablaai://acceptinvitation/?${key}
+      <br>Please <a href="${config.webappBaseUri}/app/acceptinvitation/team/${webKey}">click here</a> to join them.<br>
       `;
    return sendMail({
       from: 'habla-mailer-dev@habla.ai',
@@ -106,8 +102,7 @@ export function sendTeamRoomInviteToExistingUser(email, subscriberOrgName, teamN
    const html = `
       <h1><img src="https://static.wixstatic.com/media/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg/v1/fill/w_247,h_244,al_c,q_80,usm_0.66_1.00_0.01/ac0e25_95ce977831a9430989f049b46928fda6~mv2.jpg" height="100" width="100" align="middle"></h1>
       <br>${byUserDisplayName} has invited you to team room "${teamRoomName}" of "${subscriberOrgName}" in Habla AI.<br>
-      <br>If reading this from a web browser, please <a href="${config.webappBaseUri}/app/acceptinvitation/teamRoom/${webKey}">click here</a> to join them.<br>
-      <br>If reading this from an iOS device, please tap on the following link to join them:  hablaai://acceptinvitation/?${key}
+      <br>Please <a href="${config.webappBaseUri}/app/acceptinvitation/teamRoom/${webKey}">click here</a> to join them.<br>
       `;
    return sendMail({
       from: 'habla-mailer-dev@habla.ai',
