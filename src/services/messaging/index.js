4663503a52dafad23bbc1d74d47b372013b4a9cd
Anthony Daga <anthony.daga@habla.io>
2017-11-15 12:51:10 -0800

    Bug fix to adding subscriberUserId/teamMemberId/teamRoomMemberId and role to subscriberAdded/teamMemberAdded/teamRoomMemberAdded events.

--- src/services/messaging/index.js
+++ src/services/messaging/index.js
@@ -68,7 +68,7 @@ export const subscriberAdded = (req, subscriberOrgId, user, role, subscriberUser
 
    return _joinChannels(req, user.userId, channels)
       .then(() => {
-         const mergedUser = _.merge(user, { role, subscriberUserId });
+         const mergedUser = _.merge(user.userInfo, { userId: user.userId, role, subscriberUserId });
          _broadcastEvent(req, EventTypes.subscriberAdded, publishByApiVersion(req, apiVersionedVisibility.publicSubscriber, subscriberOrgId, mergedUser), [subscriberOrgChannel]);
       })
       .catch(err => req.logger.error(err));
@@ -109,7 +109,7 @@ export const teamMemberAdded = (req, teamId, user, role, teamMemberId) => {
 
    return _joinChannels(req, user.userId, channels)
       .then(() => {
-         const mergedUser = _.merge(user, { role, teamMemberId });
+         const mergedUser = _.merge(user.userInfo, { user: user.userId, role, teamMemberId });
          _broadcastEvent(req, EventTypes.teamMemberAdded, publishByApiVersion(req, apiVersionedVisibility.publicTeamMember, teamId, mergedUser), [teamChannel]);
       })
       .catch(err => req.logger.error(err));
@@ -150,7 +150,7 @@ export const teamRoomMemberAdded = (req, teamRoomId, user, role, teamRoomMemberI
 
    return _joinChannels(req, user.userId, channels)
       .then(() => {
-         const mergedUser = _.merge(user, { role, teamRoomMemberId });
+         const mergedUser = _.merge(user.userInfo, { userId: user.userId, role, teamRoomMemberId });
          _broadcastEvent(req, EventTypes.teamRoomMemberAdded, publishByApiVersion(req, apiVersionedVisibility.publicTeamRoomMember, teamRoomId, mergedUser), [teamRoomChannel]);
       })
       .catch(err => req.logger.error(err));
