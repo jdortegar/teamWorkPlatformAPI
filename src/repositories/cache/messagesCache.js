343d23ed0bffbfad192229f10bf5e7afb0ae8d01
Anthony Daga <anthony.daga@habla.io>
2017-12-20 20:25:24 -0800

    Test/debug messages cache layer.

--- src/repositories/cache/messagesCache.js
+++ src/repositories/cache/messagesCache.js
@@ -1,11 +1,13 @@
-// {conversationId}#conversationId#messages = { messageCount, lastTimestamp }
+// {conversationId}#conversationId#messages = { messageCount, lastTimestamp, byteCount }
 // {conversationId}#conversationId#parentMessageIds = [ parenetMesageId [, ...] ] (Redis SET)
 // {conversationId}#conversationId#{parentMessageId} = { messageCount, lastTimestamp }
+
+import config from '../../config/env';
 
 export const getRecursiveMessageCountAndLastTimestampByConversationId = (req, conversationId) => {
    return new Promise((resolve, reject) => {
       let ret;
-      req.app.locals.redis.hmgetAsync(`${conversationId}#onversationId#messages`, 'messageCount', 'lastTimestamp')
+      req.app.locals.redis.hmgetAsync(`${config.redisPrefix}${conversationId}#conversationId#messages`, 'messageCount', 'lastTimestamp', 'byteCount')
          .then((values) => {
             if (values[0] === null) {
                return undefined;
@@ -14,10 +16,11 @@ export const getRecursiveMessageCountAndLastTimestampByConversationId = (req, co
             ret = {
                conversationId,
                messageCount: values[0],
-               lastTimestamp: values[1]
+               lastTimestamp: values[1],
+               byteCount: values[2]
             };
 
-            return req.app.locals.redis.smembersAsync(`${conversationId}#conversationId#parentMessageIds`);
+            return req.app.locals.redis.smembersAsync(`${config.redisPrefix}${conversationId}#conversationId#parentMessageIds`);
          })
          .then((parentMessageIds) => {
             if (parentMessageIds === undefined) {
@@ -26,7 +29,7 @@ export const getRecursiveMessageCountAndLastTimestampByConversationId = (req, co
 
             const promises = [];
             parentMessageIds.forEach((parentMessageId) => {
-               promises.push(req.app.locals.redis.hmgetAsync(`${conversationId}#onversationId#${parentMessageId}`, 'messageCount', 'lastTimestamp')
+               promises.push(req.app.locals.redis.hmgetAsync(`${config.redisPrefix}${conversationId}#conversationId#${parentMessageId}`, 'messageCount', 'lastTimestamp')
                   .then((values) => {
                      ret[parentMessageId] = {
                         messageCount: values[0],
@@ -35,55 +38,68 @@ export const getRecursiveMessageCountAndLastTimestampByConversationId = (req, co
                   })
                );
             });
-            return promises;
+            return Promise.all(promises);
          })
          .then(() => resolve(ret))
          .catch(err => reject(err));
    });
 };
 
-export const incrementMessageCountAndLastTimestamp = (req, lastTimestamp, conversationId, parentMessageId = undefined) => {
+export const incrementMessageCountAndLastTimestampAndByteCount = (req, lastTimestamp, byteCount, conversationId, parentMessageId = undefined) => {
    return new Promise((resolve, reject) => {
-      let hashKey;
+      const messagesHashKey = `${config.redisPrefix}${conversationId}#conversationId#messages`;
+      let hashKey = messagesHashKey;
       if (parentMessageId) {
-         hashKey = `${conversationId}#conversationId#${parentMessageId}`;
-      } else {
-         hashKey = `${conversationId}#conversationId#messages`;
+         hashKey = `${config.redisPrefix}${conversationId}#conversationId#${parentMessageId}`;
       }
 
-      req.app.locals.redis.hset(hashKey, 'lastTimestamp', lastTimestamp)
-         .then(() => req.app.locals.redis.hmincrbyAsync(hashKey, 1))
+      req.app.locals.redis.hsetAsync(hashKey, 'lastTimestamp', lastTimestamp)
+         .then((created) => {
+            const promises = [];
+            if ((parentMessageId) && (created === 1)) {
+               const parentsHashKey = `${config.redisPrefix}${conversationId}#conversationId#parentMessageIds`;
+               promises.push(req.app.locals.redis.saddAsync(parentsHashKey, parentMessageId));
+            }
+            promises.push(req.app.locals.redis.hincrbyAsync(hashKey, 'messageCount', 1));
+            promises.push(req.app.locals.redis.hincrbyAsync(messagesHashKey, 'byteCount', byteCount));
+            return Promise.all(promises);
+         })
          .then(() => resolve())
          .catch(err => reject(err));
    });
 };
 
-export const setMessageCountAndLastTimestampIfNotExist = (req, messageCount, lastTimestamp, conversationId, parentMessageId = undefined) => {
+export const setMessageCountAndLastTimestampAndByteCountIfNotExist = (req, messageCount, lastTimestamp, byteCount, conversationId, parentMessageId = undefined) => {
    return new Promise((resolve, reject) => {
-      let hashKey;
+      let isNew = false;
+      const messagesHashKey = `${config.redisPrefix}${conversationId}#conversationId#messages`;
+      let hashKey = messagesHashKey;
       if (parentMessageId) {
-         hashKey = `${conversationId}#conversationId#${parentMessageId}`;
-      } else {
-         hashKey = `${conversationId}#conversationId#messages`;
+         hashKey = `${config.redisPrefix}${conversationId}#conversationId#${parentMessageId}`;
       }
 
-      req.app.locals.redis.hmexistsAsync(hashKey, 'messageCount')
+      req.app.locals.redis.hexistsAsync(hashKey, 'messageCount')
          .then((exists) => {
             if (exists === 0) {
-               const parentsHashKey = `${conversationId}#conversationId#parentMessageIds}`;
-               return Promise.all([
+               isNew = true;
+               const promises = [
                   req.app.locals.redis.hmsetAsync(hashKey,
                      'messageCount', messageCount,
-                     'lastTimestamp', lastTimestamp),
-                  req.app.locals.redis.saddAsync(parentsHashKey, parentMessageId)
-               ]);
+                     'lastTimestamp', lastTimestamp,
+                     'byteCount', byteCount),
+                  req.app.locals.redis.hincrbyAsync(messagesHashKey, 'byteCount', byteCount)
+               ];
+
+               if (parentMessageId) {
+                  const parentsHashKey = `${config.redisPrefix}${conversationId}#conversationId#parentMessageIds}`;
+                  promises.push(req.app.locals.redis.saddAsync(parentsHashKey, parentMessageId));
+               }
+
+               return Promise.all(promises);
             }
             return undefined;
          })
-         .then((result) => {
-            resolve((result));
-         })
-         .then(() => resolve())
+         .then(() => resolve(isNew))
          .catch(err => reject(err));
    });
 };
