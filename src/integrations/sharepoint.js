d22ba56238632b6a9defa0ed88d741e790f1c76f
Anthony Daga <anthony.daga@habla.io>
2018-03-02 17:51:34 -0800

    Included root site in list of sites for sharepoint.

--- src/integrations/sharepoint.js
+++ src/integrations/sharepoint.js
@@ -120,13 +120,13 @@ export const getSites = (req, sharepointOrg, userAccessToken) => {
       )
          .then((response) => {
             const sites = [];
+            console.log(response.data);
             response.data.PrimaryQueryResult.RelevantResults.Table.Rows.forEach(({ Cells }) => {
                Cells.forEach((cell) => {
                   if ((cell.Key === 'SPWebUrl') && (cell.ValueType === 'Edm.String')) {
                      const value = cell.Value;
-                     const toks = value.split('.sharepoint.com/sites/');
-                     if ((toks.length > 1) && (toks[1] !== 'contentTypeHub')) {
-                        sites.push(toks[1]);
+                     if (!value.endsWith('contentTypeHub')) {
+                        sites.push(value);
                      }
                   }
                });
