f8c7b38b1328a178ed7b759318d5de68e3041fb4
Jonatan Frank <jonafrank@gmail.com>
2018-05-02 15:54:28 -0300

    Development credentials for google

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -34,8 +34,8 @@ export const boxClientSecret = '9p20xotNgLqLF2uS6ybg1ANd53jpq3GE';
 export const boxWebhooksPrimaryKey = 'fWo4rJNBbVeSpQ0llAdiM5GnDer1CPLE';
 export const boxWebhooksSecondaryKey = 'Q5Ht4J6nucMSxTnXKRKk1w9LwJaBefLY';
 
-export const googleClientId = '56229434385-er6slkc68oaklfs58oi5ej2fbbjabpc7.apps.googleusercontent.com';
-export const googleClientSecret = 'tQ-XvOvgGmAAJfjMWjH4Ai1T';
+export const googleClientId = '801943186202-5cp3slnr8mi8vmtdruiessk3i5ugneg0.apps.googleusercontent.com';
+export const googleClientSecret = 'VPDsaJ5aAKs9gNt4y1CQBl1Z';
 
 export const sharepointClientId = 'a5a13f17-353a-493c-8121-f0659cd32ab4';
 export const sharepointClientSecret = 'cYk8V8WFLk/zaa7C8G8q2bt8UV2Tun6zoZqA4rhL8Og=';
