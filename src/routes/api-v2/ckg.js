e1e58678af4a040bc7522d30224eff6deb0251ac
Jonatan Frank <jonafrank@gmail.com>
2018-08-06 17:29:26 -0300

    include PUT endopoint to filtering things

--- src/routes/api-v2/ckg.js
+++ src/routes/api-v2/ckg.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import * as ckg from '../../controllers/api-v2/ckg';
+import { apiVersionedValidators, validateByApiVersion } from '../../config/param-validation';
 
 const router = express.Router();
 
 router.route('/:subscriberOrgId/files/:search')
     .get(ckg.getFiles);
+
+router.route('/:subscriberOrgId/files/:search')
+    .put(validateByApiVersion(apiVersionedValidators.queryFiles), ckg.putQueryFiles);
 
 export default router;
