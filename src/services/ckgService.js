38a7e4c8bfc7c1aedb30548085af1b328db1fb55
Jonatan Frank <jonafrank@gmail.com>
2018-09-10 21:34:09 -0300

    [HAB-1473] Include hablauserid and habla user name in unified endpoint

--- src/services/ckgService.js
+++ src/services/ckgService.js
@@ -9,39 +9,42 @@ const getFiles = async (query, neo4jSession, fileRecords=[]) => {
     const ownFilesInsideFolders = `
        MATCH (u:User)-[:OWNS]->(folder:Folder)-[:HAS *0..]->(f:File)
        WHERE u.subscriberOrgId = '${subscriberOrgId}' AND u.active = true
-       RETURN DISTINCT f`;
+       RETURN DISTINCT f, u`;
     fileRecords = await getFiles(ownFilesInsideFolders, neo4jSession);
  
     const ownFiles = `
        MATCH (u:User)-[:OWNS]->(f:File)
        WHERE u.subscriberOrgId = '${subscriberOrgId}' AND u.active = true
-       RETURN DISTINCT f`;
+       RETURN DISTINCT f, u`;
     fileRecords = await getFiles(ownFiles, neo4jSession, fileRecords);
  
     const shareFilesInsideFolders = `
        MATCH (f:File)<-[:HAS *0..]-(folder:Folder)-[:SHARE_WITH]->(u:User)
        WHERE u.subscriberOrgId = '${subscriberOrgId}' AND u.active = true
-       RETURN DISTINCT f`;
+       RETURN DISTINCT f, u`;
     fileRecords = await getFiles(shareFilesInsideFolders, neo4jSession, fileRecords);
  
     const shareFiles = `
        MATCH (f:File)-[:SHARE_WITH]->(u:User)
        WHERE u.subscriberOrgId = '${subscriberOrgId}' AND u.active = true
-       RETURN DISTINCT f`;
+       RETURN DISTINCT f, u`;
     fileRecords = await getFiles(shareFiles, neo4jSession, fileRecords);
  
     const files = fileRecords.map((record) => {
        const fileNode = record.get(0);
-       return {
-          fileId: fileNode.properties.fileId,
-          fileName: fileNode.properties.fileName,
-          fileSize: fileNode.properties.fileSize,
-          fileType: fileNode.properties.fileType,
-          fileSource: fileNode.properties.source,
-          fileExtension: fileNode.properties.fileExtension,
-          resourceUri: fileNode.properties.resourceUri,
-          lastModified: fileNode.properties.lastModified,
-       };
+       const userNode = record.get(1);
+        return {
+            fileId: fileNode.properties.fileId,
+            fileName: fileNode.properties.fileName,
+            fileSize: fileNode.properties.fileSize,
+            fileType: fileNode.properties.fileType,
+            fileSource: fileNode.properties.source,
+            fileExtension: fileNode.properties.fileExtension,
+            resourceUri: fileNode.properties.resourceUri,
+            lastModified: fileNode.properties.lastModified,
+            fileOwnerName: fileNode.properties.ownerName,
+            fileOwnerId: userNode.properties.hablaUserId,
+        };
     });
     return files;
  };
