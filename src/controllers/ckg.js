0f44e31ec3a11871d412bce6a88537b570841f0d
Kelly Qian <kqian@Kellys-MBP.home.maxqia.net>
2018-10-22 09:54:30 -0700

    Team Level Smart Search

--- src/controllers/ckg.js
+++ src/controllers/ckg.js
@@ -1,11 +1,18 @@
 import httpStatus from 'http-status';
 import * as ckgSvc from '../services/ckgService';
+import * as ckgTeamSvc from '../services/ckgTeamService';
 
 export const getFiles = async (req, res) => {
    const { neo4jSession } = req.app.locals;
-   const { subscriberOrgId } = req.params;
+   const { subscriberOrgId } = req.params.subscriberOrgId;
+   const { subscriberTeamId } = req.params.subscriberTeamId;
+   var files = null;
 
-   const files = await ckgSvc.getFilesBySubscriberOrgId(neo4jSession, subscriberOrgId)
+   if (subscriberTeamId) {
+       files = await ckgTeamSvc.getFilesBySubscriberTeamId(neo4jSession, subscriberTeamId)
+   } else {
+    files = await ckgSvc.getFilesBySubscriberOrgId(neo4jSession, subscriberOrgId)
+   }
 
    return res.status(httpStatus.OK).json({
       message: {
@@ -19,14 +26,21 @@ export const getFiles = async (req, res) => {
 export const getFilesBySearchTerm = async (req, res) => {
     const { neo4jSession } = req.app.locals;
     const subscriberOrgId = req.params.subscriberOrgId;
+    const subscriberTeamId = req.params.subscriberTeamId;
     const searchTerm = req.params.searchTerm;
     const caseInsensitive = req.params.caseInsensitive || 1;
     const andOperator = req.params.andOperator || 0;
 
     console.log("andOperator="+andOperator);
 
-    const files = await ckgSvc.getFilesBySubscriberOrgIdSearchTerm(neo4jSession, subscriberOrgId, searchTerm, caseInsensitive, andOperator)
- 
+    var files = null;
+
+    if (subscriberTeamId) {
+        files = await ckgSvc.getFilesBySubscriberOrgIdSearchTerm(neo4jSession, subscriberTeamId, searchTerm, caseInsensitive, andOperator)
+    } else {
+        files = await ckgSvc.getFilesBySubscriberOrgIdSearchTerm(neo4jSession, subscriberOrgId, searchTerm, caseInsensitive, andOperator)
+    }
+
     return res.status(httpStatus.OK).json({
        message: {
           fileTypes: [],
