6c68c47b8f4cd29397b27a0f8b7298500566d30a
Kelly Qian <kqian@Kellys-MBP.home.maxqia.net>
2018-10-23 13:22:25 -0700

    Modify CKG & Smart Search to support both Organization level and Team level

--- src/controllers/ckg.js
+++ src/controllers/ckg.js
@@ -4,14 +4,20 @@ import * as ckgTeamSvc from '../services/ckgTeamService';
 
 export const getFiles = async (req, res) => {
    const { neo4jSession } = req.app.locals;
-   const { subscriberOrgId } = req.params.subscriberOrgId;
-   const { subscriberTeamId } = req.params.subscriberTeamId;
+   const subscriberOrgId = req.params.subscriberOrgId;
+   const subscriberTeamId = req.params.subscriberTeamId;
    var files = null;
 
-   if (subscriberTeamId !== null && subscriberTeamId !== '' && subscriberTeamId !== 0) {
+   if (subscriberTeamId !== null && subscriberTeamId != 0) {
+       console.log("subscriberOrgId=" +subscriberOrgId);
+       console.log("subscriberTeamId=" +subscriberTeamId);
+       console.log("Team Level");
        files = await ckgTeamSvc.getFilesBySubscriberTeamId(neo4jSession, subscriberTeamId)
    } else {
-    files = await ckgSvc.getFilesBySubscriberOrgId(neo4jSession, subscriberOrgId)
+       console.log("subscriberOrgId=" +subscriberOrgId);
+       console.log("subscriberTeamId=" +subscriberTeamId);
+       console.log("Organization Level");
+       files = await ckgSvc.getFilesBySubscriberOrgId(neo4jSession, subscriberOrgId)
    }
 
    return res.status(httpStatus.OK).json({
@@ -35,9 +41,15 @@ export const getFilesBySearchTerm = async (req, res) => {
 
     var files = null;
 
-    if (subscriberTeamId !== null && subscriberTeamId !== '' && subscriberTeamId !== 0) {
+    if (subscriberTeamId !== null && subscriberTeamId != 0) {
+        console.log("subscriberOrgId=" +subscriberOrgId);
+        console.log("subscriberTeamId=" +subscriberTeamId);
+        console.log("Team Level");
         files = await ckgTeamSvc.getFilesBySubscriberTeamIdSearchTerm(neo4jSession, subscriberTeamId, searchTerm, caseInsensitive, andOperator)
     } else {
+        console.log("subscriberOrgId=" +subscriberOrgId);
+        console.log("subscriberTeamId=" +subscriberTeamId);
+        console.log("Organization Level");
         files = await ckgSvc.getFilesBySubscriberOrgIdSearchTerm(neo4jSession, subscriberOrgId, searchTerm, caseInsensitive, andOperator)
     }
 
