6b9311fb0ba73893a1bb94d8eee7fab68a9b8fac
Jonatan Frank <jonafrank@gmail.com>
2019-01-22 19:41:37 -0300

    fix conflicts:w

--- src/routes/api-v2/index.js
+++ src/routes/api-v2/index.js
@@ -5,11 +5,8 @@ import integrationsV2Routes from './integrations';
 import teamRoutes from './teams';
 import organization from './organization';
 import stripeRoutes from './stripe';
-<<<<<<< HEAD
 import conversationsRoutes from './conversations';
-=======
 import paypalRoutes from './paypal';
->>>>>>> develop
 
 const router = express.Router();
 
@@ -19,10 +16,7 @@ router.use(surveyRoutes);
 router.use(integrationsV2Routes);
 router.use(teamRoutes);
 router.use(stripeRoutes);
-<<<<<<< HEAD
 router.use(conversationsRoutes);
-=======
 router.use(paypalRoutes);
->>>>>>> develop
 
 export default router;
