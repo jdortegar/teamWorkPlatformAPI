5113b02dbd0e2b64b5445d921a9f18e3afbbb7e2
Jonatan Frank <jonafrank@gmail.com>
2019-05-24 19:14:16 -0300

    add debug code

--- src/controllers/ckg.js
+++ src/controllers/ckg.js
@@ -30,34 +30,40 @@ export const getFiles = async (req, res) => {
 };
 
 export const getFilesBySearchTerm = async (req, res) => {
-    const { neo4jSession } = req.app.locals;
-    const subscriberOrgId = req.params.subscriberOrgId;
-    const subscriberTeamId = req.params.subscriberTeamId;
-    const searchTerm = req.params.searchTerm;
-    const caseInsensitive = req.params.caseInsensitive || 1;
-    const andOperator = req.params.andOperator || 0;
+   try {
 
-    console.log("andOperator="+andOperator);
-
-    var files = null;
-
-    if (subscriberTeamId !== null && subscriberTeamId != 0) {
-        console.log("subscriberOrgId=" +subscriberOrgId);
-        console.log("subscriberTeamId=" +subscriberTeamId);
-        console.log("Team Level");
-        files = await ckgTeamSvc.getFilesBySubscriberTeamIdSearchTerm(neo4jSession, subscriberTeamId, searchTerm, caseInsensitive, andOperator)
-    } else {
-        console.log("subscriberOrgId=" +subscriberOrgId);
-        console.log("subscriberTeamId=" +subscriberTeamId);
-        console.log("Organization Level");
-        files = await ckgSvc.getFilesBySubscriberOrgIdSearchTerm(neo4jSession, subscriberOrgId, searchTerm, caseInsensitive, andOperator)
-    }
-
-    return res.status(httpStatus.OK).json({
-       message: {
-          fileTypes: [],
-          files,
-          edges: []
-       }
-    });
+      const { neo4jSession } = req.app.locals;
+      const subscriberOrgId = req.params.subscriberOrgId;
+      const subscriberTeamId = req.params.subscriberTeamId;
+      const searchTerm = req.params.searchTerm;
+      const caseInsensitive = req.params.caseInsensitive || 1;
+      const andOperator = req.params.andOperator || 0;
+  
+      console.log("andOperator="+andOperator);
+  
+      var files = null;
+  
+      if (subscriberTeamId !== null && subscriberTeamId != 0) {
+          console.log("subscriberOrgId=" +subscriberOrgId);
+          console.log("subscriberTeamId=" +subscriberTeamId);
+          console.log("Team Level");
+          files = await ckgTeamSvc.getFilesBySubscriberTeamIdSearchTerm(neo4jSession, subscriberTeamId, searchTerm, caseInsensitive, andOperator)
+      } else {
+          console.log("subscriberOrgId=" +subscriberOrgId);
+          console.log("subscriberTeamId=" +subscriberTeamId);
+          console.log("Organization Level");
+          files = await ckgSvc.getFilesBySubscriberOrgIdSearchTerm(neo4jSession, subscriberOrgId, searchTerm, caseInsensitive, andOperator)
+      }
+  
+      return res.status(httpStatus.OK).json({
+         message: {
+            fileTypes: [],
+            files,
+            edges: []
+         }
+      });
+   } catch (err) {
+      console.error('*****ERROR', err);
+      return Promise.reject(err);
+   }
  };
