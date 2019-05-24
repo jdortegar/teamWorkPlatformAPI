b7ebbf6cf52b1541799b8eced839ca1a23cca2e8
Jonatan Frank <jonafrank@gmail.com>
2019-05-24 19:44:04 -0300

    add more try catches

--- src/services/ckgTeamService.js
+++ src/services/ckgTeamService.js
@@ -10,49 +10,54 @@ const getFiles = async (query, neo4jSession, fileRecords = []) => {
 
 
 export const getFilesBySubscriberTeamId = async (neo4jSession, subscriberTeamId) => {
-   let fileRecords = [];
-   const ownFilesInsideFolders = `
-       MATCH (u:User)-[:OWNS]->(folder:Folder)-[:HAS *0..]->(f:File)
-       WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
-       RETURN DISTINCT f, u`;
-   fileRecords = await getFiles(ownFilesInsideFolders, neo4jSession);
+   try {
 
-   const ownFiles = `
-       MATCH (u:User)-[:OWNS]->(f:File)
-       WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
-       RETURN DISTINCT f, u`;
-   fileRecords = await getFiles(ownFiles, neo4jSession, fileRecords);
-
-   const shareFilesInsideFolders = `
-       MATCH (f:File)<-[:HAS *0..]-(folder:Folder)-[:SHARE_WITH]->(u:User)
-       WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
-       RETURN DISTINCT f, u`;
-   fileRecords = await getFiles(shareFilesInsideFolders, neo4jSession, fileRecords);
-
-   const shareFiles = `
-       MATCH (f:File)-[:SHARE_WITH]->(u:User)
-       WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
-       RETURN DISTINCT f, u`;
-   fileRecords = await getFiles(shareFiles, neo4jSession, fileRecords);
-
-   const files = fileRecords.map((record) => {
-      const fileNode = record.get(0);
-      const userNode = record.get(1);
-      return {
-         fileId: fileNode.properties.fileId,
-         fileName: fileNode.properties.fileName,
-         fileSize: fileNode.properties.fileSize,
-         fileType: fileNode.properties.fileType,
-         fileSource: fileNode.properties.source,
-         fileExtension: fileNode.properties.fileExtension,
-         resourceUri: fileNode.properties.resourceUri,
-         lastModified: fileNode.properties.lastModified,
-         fileOwnerName: fileNode.properties.ownerName,
-         fileOwnerId: userNode.properties.hablaUserId,
-         fileCreatedAt: fileNode.properties.createdAt,
-      };
-   });
-   return files;
+      let fileRecords = [];
+      const ownFilesInsideFolders = `
+          MATCH (u:User)-[:OWNS]->(folder:Folder)-[:HAS *0..]->(f:File)
+          WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
+          RETURN DISTINCT f, u`;
+      fileRecords = await getFiles(ownFilesInsideFolders, neo4jSession);
+   
+      const ownFiles = `
+          MATCH (u:User)-[:OWNS]->(f:File)
+          WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
+          RETURN DISTINCT f, u`;
+      fileRecords = await getFiles(ownFiles, neo4jSession, fileRecords);
+   
+      const shareFilesInsideFolders = `
+          MATCH (f:File)<-[:HAS *0..]-(folder:Folder)-[:SHARE_WITH]->(u:User)
+          WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
+          RETURN DISTINCT f, u`;
+      fileRecords = await getFiles(shareFilesInsideFolders, neo4jSession, fileRecords);
+   
+      const shareFiles = `
+          MATCH (f:File)-[:SHARE_WITH]->(u:User)
+          WHERE u.teamId = '${subscriberTeamId}' AND u.active = true
+          RETURN DISTINCT f, u`;
+      fileRecords = await getFiles(shareFiles, neo4jSession, fileRecords);
+   
+      const files = fileRecords.map((record) => {
+         const fileNode = record.get(0);
+         const userNode = record.get(1);
+         return {
+            fileId: fileNode.properties.fileId,
+            fileName: fileNode.properties.fileName,
+            fileSize: fileNode.properties.fileSize,
+            fileType: fileNode.properties.fileType,
+            fileSource: fileNode.properties.source,
+            fileExtension: fileNode.properties.fileExtension,
+            resourceUri: fileNode.properties.resourceUri,
+            lastModified: fileNode.properties.lastModified,
+            fileOwnerName: fileNode.properties.ownerName,
+            fileOwnerId: userNode.properties.hablaUserId,
+            fileCreatedAt: fileNode.properties.createdAt,
+         };
+      });
+      return files;
+   } catch (err) {
+      return Promise.reject(err);
+   }
 };
 
 
