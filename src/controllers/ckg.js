f7ddba382f8ec50a1e36c54f3900e55dae053d97
Jonatan Frank <jonafrank@gmail.com>
2019-05-24 20:27:18 -0300

    add more try catches

--- src/controllers/ckg.js
+++ src/controllers/ckg.js
@@ -2,31 +2,36 @@ import httpStatus from 'http-status';
 import * as ckgSvc from '../services/ckgService';
 import * as ckgTeamSvc from '../services/ckgTeamService';
 
-export const getFiles = async (req, res) => {
-   const { neo4jSession } = req.app.locals;
-   const subscriberOrgId = req.params.subscriberOrgId;
-   const subscriberTeamId = req.params.subscriberTeamId;
-   var files = null;
-
-   if (subscriberTeamId !== null && subscriberTeamId != 0) {
-       console.log("subscriberOrgId=" +subscriberOrgId);
-       console.log("subscriberTeamId=" +subscriberTeamId);
-       console.log("Team Level");
-       files = await ckgTeamSvc.getFilesBySubscriberTeamId(neo4jSession, subscriberTeamId)
-   } else {
-       console.log("subscriberOrgId=" +subscriberOrgId);
-       console.log("subscriberTeamId=" +subscriberTeamId);
-       console.log("Organization Level");
-       files = await ckgSvc.getFilesBySubscriberOrgId(neo4jSession, subscriberOrgId)
-   }
-
-   return res.status(httpStatus.OK).json({
-      message: {
-         fileTypes: [],
-         files,
-         edges: []
+export const getFiles = async (req, res, next) => {
+   try {
+      const { neo4jSession } = req.app.locals;
+      const subscriberOrgId = req.params.subscriberOrgId;
+      const subscriberTeamId = req.params.subscriberTeamId;
+      var files = null;
+   
+      if (subscriberTeamId !== null && subscriberTeamId != 0) {
+          console.log("subscriberOrgId=" +subscriberOrgId);
+          console.log("subscriberTeamId=" +subscriberTeamId);
+          console.log("Team Level");
+          files = await ckgTeamSvc.getFilesBySubscriberTeamId(neo4jSession, subscriberTeamId)
+      } else {
+          console.log("subscriberOrgId=" +subscriberOrgId);
+          console.log("subscriberTeamId=" +subscriberTeamId);
+          console.log("Organization Level");
+          files = await ckgSvc.getFilesBySubscriberOrgId(neo4jSession, subscriberOrgId)
       }
-   });
+   
+      return res.status(httpStatus.OK).json({
+         message: {
+            fileTypes: [],
+            files,
+            edges: []
+         }
+      });
+   } catch (err) {
+      console.error('****ERROR', err);
+      next(err);
+   }
 };
 
 export const getFilesBySearchTerm = async (req, res) => {
