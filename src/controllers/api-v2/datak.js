59b0097a2e604aa691b69928e7de09ddaa8e251d
seanchen0822 <40006782+seanchen0822@users.noreply.github.com>
2019-06-13 09:38:21 -0700

    Revert "combine 3 types of data in one enpoint"

--- src/controllers/api-v2/datak.js
+++ src/controllers/api-v2/datak.js
@@ -44,19 +44,13 @@ export const getDataFilesBySearchTerm = async (req, res) => {
         const andOperator = req.params.andOperator || 0;
 
         console.log("andOperator=" + andOperator);
-        
-        var data = null;        
+
         var files = [];
         var teams = null;
         if (hablaUserId !== null && searchTerm !== null) {
+
+            teams = await getTeamMembersByUserId(req, hablaUserId);
             
-            // getting messages and attached files
-            // and teams a user belongs to
-            [data,teams] = await Promise.all([
-                datakSvc.getDataBySearchTerm(neo4jSession, hablaUserId, searchTerm, caseInsensitive, andOperator),
-                getTeamMembersByUserId(req, hablaUserId)
-            ])
-            // getting integration files
             // get team files
             const promises = [];
             teams.forEach((item) => {
@@ -91,7 +85,6 @@ export const getDataFilesBySearchTerm = async (req, res) => {
         return res.status(httpStatus.OK).json({
             message: {
                 fileTypes: [],
-                data,
                 files,
                 edges: []
             }
